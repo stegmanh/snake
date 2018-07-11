776cb0f588c5d70d7fcac03e31472d8e7f95cf8d
Jared Allard <jaredallard@outlook.com>
2018-07-10 20:30:37 -0700

    feat: rewrite in go

--- src/ui.ts
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  const canvas: HTMLCanvasElement | null = document.getElementById("canvas") as HTMLCanvasElement | null;
-  if (canvas == null) { throw new Error("Cannot find canvas element"); }
-
-  const game = start(canvas);
-}
-
